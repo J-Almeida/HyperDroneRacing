8866f75897aa272428966685e167f5fa1a0bf296
Miguel Mendes <zephiro309@gmail.com>
2016-06-14 20:23:48 +0100

    Added test menu.

---
+++ Assets/Scripts/MenuSelectionBehaviour.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuSelectionBehaviour : MonoBehaviour {
+
+    public int currentSelectionIndex = 0;
+    public List<GameObject> options;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("joystick button 4") || Input.GetKeyDown("up")) {
+            currentSelectionIndex--;
+        }
+        if (Input.GetKeyDown("joystick button 6") || Input.GetKey("down")) {
+            currentSelectionIndex++;
+        }
+        if (Input.GetKeyDown("joystick button 13")) {
+            Application.LoadLevel(1);
+        }
+
+        if (currentSelectionIndex >= options.Count)
+            currentSelectionIndex = 0;
+        if (currentSelectionIndex < 0)
+            currentSelectionIndex = options.Count - 1;
+
+        this.transform.position = options[currentSelectionIndex].transform.position;
+    }
+}
